aace5d6448e8929ef120be152025992be44346f7
Raghav Sharda <raghavsharda96@gmail.com>
2020-05-18 17:33:39 -0400

    started A* , children nodes need major work

--- Graph.py
+++ Graph.py
@@ -1,27 +1,91 @@
+def astar(graphcongif, initialCoordinate, finalCoordinate):
+    
+    # Initialize both open and closed list
+    open_list = []
+    closed_list = []
+
+    # Add the start node
+    open_list.append(initialCoordinate)
+
+    # Loop until you find the end
+    while len(open_list) > 0:
+        
+        # Get the current node
+        current = min(open_list, key=lambda o:o.G + o.H)
+
+        #If it is the item we want, retrace the path and return it
+        if current.id == finalCoordinate.id:
+            path = []
+            while current.parent:
+                path.append(current)
+                current = current.parent
+            path.append(current)
+            return path[::-1]
+        
+        #Remove the item from the open set
+        open_list.remove(current)
+
+        #Add it to the closed set
+        closed_list.append(current)
+
+        # Loop through the node's children/neighbour
+        for direction,coordinatesOfChildren in current.neighbours.items():
+            #If the children is already in the closed set, skip it
+            for node in closed_list:
+                if (coordinatesOfChildren['xCoordinate'] == node.xCoordinate) and (coordinatesOfChildren['yCoordinate'] == node.yCoordinate):
+                    print ("Node has already been visited")
+                    continue
+            
+            for node in open_list:
+                continue
+                # Check if we beat the G score 
+                # new_g = current.G + current.move_cost()
+                # if node.G > new_g:
+                #     #If so, update the node to have a new parent
+                #     node.G = new_g
+                #     node.parent = current
+
 class Vertex:
     def __init__(self,id,X,Y):
-
         self.id = id
+        self.parent = None
         self.xCoordinate = X
         self.yCoordinate = Y
-        self.adjacent = {}
+        self.H = 0
+        self.G = 0
+        self.F = 0
+        self.neighbours = {'north' :{},'east':{},'south':{},'west':{}}
         self.createNeighbours()
+
+    def getId(self,xCoordinate,yCoordinate):
+        return self.id
 
     def __str__(self):
         return str(self.xCoordinate),str(self.yCoordinate)
 
     def createNeighbours(self):
-
         gridSize = 2
-        eastNeighbour=self.xCoordinate+gridSize*(1/100)
+        eastNeighbour=self.xCoordinate+(gridSize*(1/100))
         westNeighbour=self.xCoordinate-(gridSize*(1/100))
         northNeighbour=self.yCoordinate+(gridSize*(1/100))
-        southNeighbour=self.yCoordinate+(gridSize*(1/100))
-       
-        self.adjacent['east'] = eastNeighbour,self.yCoordinate
-        self.adjacent['west'] = westNeighbour,self.yCoordinate
-        self.adjacent['north'] = self.xCoordinate,northNeighbour
-        self.adjacent['south'] = self.xCoordinate,southNeighbour
+        southNeighbour=self.yCoordinate-(gridSize*(1/100))
+
+        self.neighbours["north"]["xCoordinate"] = self.xCoordinate
+        self.neighbours['north']['yCoordinate'] = northNeighbour
+
+        self.neighbours['east']['xCoordinate'] = eastNeighbour
+        self.neighbours['east']['yCoordinate'] = self.yCoordinate
+
+        self.neighbours['south']['xCoordinate'] = self.xCoordinate
+        self.neighbours['south']['yCoordinate'] = southNeighbour
+
+        self.neighbours['west']['xCoordinate'] = westNeighbour
+        self.neighbours['west']['yCoordinate'] = self.yCoordinate
+
+        # self.neighbours["north"] = self.xCoordinate,northNeighbour
+        # self.neighbours['east'] = eastNeighbour,self.yCoordinate
+        # self.neighbours['south'] = self.xCoordinate,southNeighbour
+        # self.neighbours['west'] = westNeighbour,self.yCoordinate
 
         # print("East"   ,eastNeighbour,self.yCoordinate)
         # print("West"    ,westNeighbour,self.yCoordinate)
@@ -29,8 +93,7 @@ class Vertex:
         # print("South"   ,self.xCoordinate,southNeighbour)
 
     def get_Neighbours(self):
-        return self.adjacent.keys()
-
+        return self.neighbours.values()
 
 class Graph:
     def __init__(self):
@@ -49,18 +112,24 @@ class Graph:
 if __name__ == '__main__':
     
     g = Graph()
-    v = Vertex(1,-73.59,45.49)
-    g.add_vertex(v)
-    # g.add_vertex(2,-73.59,45.51)
-    # g.add_vertex(3,-73.59,45.53)
-    # g.add_vertex(4,-73.57,45.49)
+    v1 = Vertex(1,-73.59,45.49)
+    v2 = Vertex(2,-73.59,45.51)
+    # v3 = Vertex(3,-73.59,45.53)
+    # v4 = Vertex(4,-73.57,45.49)
 
-    print("Number of current Vertices " , g.num_vertices)
-    print("\n") 
-    print("Which is/are " , g.get_vertices()) 
-    print("\n") 
-    print("Its Neighbours are " , g.get_Neighbours()) 
-    print("\n") 
+    g.add_vertex(v1)
+    g.add_vertex(v2)
+    # g.add_vertex(v3)
+    # g.add_vertex(v4)
+
+    a = astar(g,v1,v2)
+    print(a)
+    # print("Number of current Vertices " , g.num_vertices)
+    # print("\n") 
+    # # print("Which is/are " , g.get_vertices()) 
+    # print("\n") 
+    # print("Its Neighbours are " , v1.get_Neighbours()) 
+    # print("\n") 
 
 
 
